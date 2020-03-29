dd9b195ffb3ee516a32df108ff8a7e813cf114fd
pawelrubin <pawelrubin19@gmail.com>
2020-03-29 19:55:35 +0200

    rsa WIP

--- lista2/rsa.py
+++ lista2/rsa.py
@@ -1,4 +1,19 @@
 import secrets
+from math import gcd
+
+
+def mod_inv(a, b):
+    s = 0
+    old_s = 1
+    r = b
+    old_r = a
+
+    while r != 0:
+        quotient = old_r // r
+        old_r, r = r, old_r - quotient * r
+        old_s, s = s, old_s - quotient * s
+
+    return old_s
 
 
 def miller_rabin(n, k=10):
@@ -36,10 +51,56 @@ def miller_rabin(n, k=10):
     return True
 
 
-def get_prime(bits):
-    lower_bound = 6074001000 << (bits - 33)
+def get_prime(bits: int):
+    lower_bound = 1 << (bits - 1)
     upper_bound = (1 << bits) - 1
     p = secrets.randbelow(upper_bound - lower_bound) + lower_bound
     while not miller_rabin(p):
         p = secrets.randbelow(upper_bound - lower_bound) + lower_bound
     return p
+
+
+def _generate_keys(keysize: int):
+    p = get_prime(keysize // 2)
+    q = get_prime(keysize // 2)
+    m = (p - 1) * (q - 1)
+
+    n = p * q
+
+    d = 2
+    while gcd(d, m) != 1:
+        d += 1
+
+    e = mod_inv(d, m)
+
+    return (d, n), (e, n)
+
+
+def encrypt(self, m: int):
+    return pow(m, self.e, self.n)
+
+
+def decrypt(self, c: int):
+    return pow(c, self.d, self.n)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gen-keys", action="store_true")
+
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument("--encrypt", action="store_true")
+    group.add_argument("--decrypt", action="store_true")
+
+    parser.add_argument_group(group)
+
+    args = parser.parse_args()
+
+    if args.gen_keys:
+        print("gen")
+    if args.encrypt:
+        print("encrypt")
+    elif args.decrypt:
+        print("decrypt")
